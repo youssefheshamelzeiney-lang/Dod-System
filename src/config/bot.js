b7c7c0e75fdcad9ce194ce34a4fc05e93cf0b184
youssefheshamelzeiney-lang <youssefheshamelzeiney@gmail.com>
2026-03-29 12:35:20 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Made By AIMBOT",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
